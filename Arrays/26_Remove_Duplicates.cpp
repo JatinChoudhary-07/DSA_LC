c899b8adaa2de10e21c06f6441114776fd46e56a
Jatin Choudhary <225982315+JatinChoudhary-07@users.noreply.github.com>
2026-04-29 15:27:07 +0530

    Add reverseWords function to reverse words in a string
    Implemented a function to reverse the words in a string using two pointers and in-place reversal.

--- Arrays/26_Remove_Duplicates.cpp
+++ Arrays/26_Remove_Duplicates.cpp
@@ -1,1 +1,34 @@
+// LC 151: Reverse Words in a String
+// Topic: Strings
+// Difficulty: Medium
+// Approach: Two Pointers + In-place
+// Time: O(n)
+// Space: O(1)
 
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        reverse(s.begin(), s.end());
+
+        int n = s.size();
+        int l = 0, r = 0, i = 0;
+
+        while (i < n) {
+            while (i < n && s[i] != ' ')
+                s[r++] = s[i++];
+
+            if (l < r) {
+                reverse(s.begin() + l, s.begin() + r);
+                s[r++] = ' ';
+                l = r;
+            }
+            i++;
+        }
+
+        return s.substr(0, r - 1);
+    }
+};
