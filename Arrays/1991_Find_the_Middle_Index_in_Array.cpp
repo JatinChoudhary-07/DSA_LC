1e2422661dc25b26b780a517b849cbf510770184
Jatin Choudhary <225982315+JatinChoudhary-07@users.noreply.github.com>
2026-06-07 10:04:44 +0530

    Create 1991_Find_the_Middle_Index_in_Array.cpp

---
+++ Arrays/1991_Find_the_Middle_Index_in_Array.cpp
@@ -0,0 +1,37 @@
+// LC 1991: Find the Middle Index in Array
+// Topic: Arrays
+// Difficulty: Easy
+// Approach: Prefix Sum Arrays
+// Time: O(n)
+// Space: O(n)
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> leftSum(n);
+        leftSum[0] = 0;
+
+        for (int i = 1; i < n; i++) {
+            leftSum[i] = leftSum[i - 1] + nums[i - 1];
+        }
+
+        vector<int> rightSum(n);
+        rightSum[n - 1] = 0;
+
+        for (int i = n - 2; i >= 0; i--) {
+            rightSum[i] = rightSum[i + 1] + nums[i + 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (leftSum[i] == rightSum[i])
+                return i;
+        }
+
+        return -1;
+    }
+};
